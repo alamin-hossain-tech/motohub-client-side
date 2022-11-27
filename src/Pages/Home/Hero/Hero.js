16d87727e54e8a21c958b3a9205ce932f1bbeb81
alamin-merndev <alaminnishat@gmail.com>
2022-11-27 12:13:02 +0600

    Hero Section Added

---
+++ src/Pages/Home/Hero/Hero.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div
+      className=" text-white text-center py-72"
+      style={{
+        backgroundImage: `url("https://motoro.wpsixer.com/motoro-spide/wp-content/uploads/2019/12/banner-bg.png")`,
+        backgroundSize: "cover",
+      }}
+    >
+      <h1 className="text-7xl  font-bold">Buy, Sell or Trade</h1>
+      <p>Find Your desire car as MotoHub makes it easy !</p>
+      <span className="bg-white text-black px-2 font-medium pb-1">
+        Browse from numerious Cars.
+      </span>
+    </div>
+  );
+};
+
+export default Hero;
