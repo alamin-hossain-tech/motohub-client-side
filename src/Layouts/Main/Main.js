32d308ae3579cc7fb2a149a18f0bb1420a4ad2ef
alamin-merndev <alaminnishat@gmail.com>
2022-11-27 11:50:38 +0600

    Necessary package installed

---
+++ src/Layouts/Main/Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import FooterMain from "../../Pages/Shared/FooterMain/FooterMain";
+import Header from "../../Pages/Shared/Header/Header";
+
+const Main = () => {
+  return (
+    <div>
+      <Header></Header>
+      <Outlet></Outlet>
+      <FooterMain></FooterMain>
+    </div>
+  );
+};
+
+export default Main;
