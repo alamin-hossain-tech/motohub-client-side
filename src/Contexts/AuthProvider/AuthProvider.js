ce50b45de59c7f61cc967a5b82ea7c7444e2a186
alamin-merndev <alaminnishat@gmail.com>
2022-11-27 13:01:23 +0600

    Firebase Config Connect

--- src/Contexts/AuthProvider/AuthProvider.js
+++ src/Contexts/AuthProvider/AuthProvider.js
@@ -1,9 +1,57 @@
-import React, { createContext } from "react";
+import React, { createContext, useEffect, useState } from "react";
+import app from "../../Firebase/.firebase.config";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
 
 export const AuthContext = createContext();
+const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
-  const authInfo = {};
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const providerLogin = (provider) => {
+    setLoading(true);
+    return signInWithPopup(auth, provider);
+  };
+
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const updateUserProfile = (profile) => {
+    return updateProfile(auth.currentUser, profile);
+  };
+
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+  }, [user]);
+
+  const authInfo = {
+    auth,
+  };
+  console.log(auth);
   return (
     <div>
       <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
--- src/Routes/PrivateRoutes/PrivateRoutes.js
+++ src/Routes/PrivateRoutes/PrivateRoutes.js
@@ -1,7 +1,24 @@
-import React from "react";
+import { Spinner } from "flowbite-react";
+import React, { useContext } from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import { AuthContext } from "../../Contexts/AuthProvider/AuthProvider";
 
-const PrivateRoutes = () => {
-  return <div></div>;
+const PrivateRoutes = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+  const location = useLocation();
+
+  if (loading) {
+    return (
+      <div className="text-center">
+        <Spinner aria-label="Center-aligned spinner example" />
+      </div>
+    );
+  }
+
+  if (!user) {
+    return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
+  }
+  return children;
 };
 
 export default PrivateRoutes;
